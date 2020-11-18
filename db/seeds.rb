61b09a6053b92c30622f569a13f92b76daec0754
Marcel Fonseca <marcelsf23br@gmail.com>
2020-11-18 19:04:13 -0300

    prepared for livecode with new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,30 @@ puts "Cleaning our db..."
 
 Garden.destroy_all
 
-
 new_garden = Garden.create(name: "My little garden", banner_url: 'https://images.unsplash.com/photo-1585320806297-9794b3e4eeae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80')
 
+other = Garden.create!(
+  name: "My Other Garden",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
+)
 
-puts "#{new_garden.name} was created"
+Plant.create!(
+  name: "Monstera",
+  image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/monstera.jpg",
+  garden: new_garden
+)
+
+Plant.create!(
+  name: "Philo",
+  image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/philo.jpg",
+  garden: new_garden
+)
+
+Plant.create!(
+  name: "Dieff",
+  image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/dieffenbachia.jpg",
+  garden: other
+)
+
+puts "Gardens and plants have been created"
+
